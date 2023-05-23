7f374cd00efb1f3af24dee7540f6de2c6831e4a7
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-05-23 15:21:05 -0600

    Changes

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+
+function App(){
+  return(
+    <div>
+      <LinkList />
+      <CreateLink />
+    </div>
+  )
+  };
+
+
+export default App;
